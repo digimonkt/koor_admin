693e4a20143de6aaff5f993b275f720e39abe4ff
sanskar <sahup3296@gmail.com>
2023-11-27 13:02:04 +0530

    env fix

--- src/utils/serverUrl.js
+++ src/utils/serverUrl.js
@@ -2,4 +2,3 @@ import env from "./validateEnv";
 
 export const SERVER_URL = env.REACT_APP_BACKEND_URL;
 export const FILE_BASE_URL = env.REACT_APP_BACKEND_URL;
-export const WEBSOCKET_URL = env.REACT_APP_WS_BACKEND_URL;
--- src/utils/validateEnv.js
+++ src/utils/validateEnv.js
@@ -2,7 +2,6 @@ import { cleanEnv, str } from "envalid";
 const env = cleanEnv(process.env, {
   REACT_APP_BACKEND_URL: str(),
   REACT_APP_REDIRECT_URL: str(),
-  REACT_APP_WS_BACKEND_URL: str(),
 });
 
 export default env;
